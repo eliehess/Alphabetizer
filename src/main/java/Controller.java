f2ad83fd1d238643148f25be1690a94184eb75ae
Elie <eliemhess@gmail.com>
2019-10-20 19:43:15 -0400

    Condense further

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -16,9 +16,36 @@ public class Controller {
     @FXML
     private void alphabetize() {
         String text = preTextArea.getText();
-        List<Card> cards = makeCards(text);
+        List<Card> cards = new ArrayList<>();
 
-        if (cards == null) return;
+        final String splitDelim = "!";
+        if (text.contains(splitDelim)) return;
+
+        String replaced = text.replaceAll("\\r", "").replaceAll("\\n", splitDelim);
+
+        StringTokenizer strtok = new StringTokenizer(replaced, splitDelim);
+
+        while (strtok.hasMoreTokens()) {
+            String str = strtok.nextToken();
+            Card card = new Card();
+
+            final int cardDelim = str.indexOf(" ");
+
+            String cardNum = str.substring(0, cardDelim);
+            if (cardNum.substring(cardNum.length() - 1).equals("x"))
+                cardNum = cardNum.substring(0, cardNum.length() - 1);
+
+            try {
+                card.quantity = Integer.parseInt(cardNum);
+            } catch (NumberFormatException e) {
+                System.out.println("NumberFormatException detected: " + e);
+                return;
+            }
+
+            card.name = str.substring(1 + cardDelim);
+            cards.add(card);
+        }
+
         Collections.sort(cards);
 
         String fin = "";
@@ -41,39 +68,5 @@ public class Controller {
         public int compareTo(Card c) {
             return name.compareTo(c.name);
         }
-    }
-
-    private static List<Card> makeCards(String text) {
-        List<Card> fin = new ArrayList<>();
-
-        final String splitDelim = "!";
-        if (text.contains(splitDelim)) return null;
-
-        String replaced = text.replaceAll("\\r", "").replaceAll("\\n", splitDelim);
-
-        StringTokenizer strtok = new StringTokenizer(replaced, splitDelim);
-
-        while (strtok.hasMoreTokens()) {
-            String str = strtok.nextToken();
-            Card card = new Card();
-
-            final int cardDelim = str.indexOf(" ");
-
-            String cardNum = str.substring(0, cardDelim);
-            if (cardNum.substring(cardNum.length() - 1).equals("x"))
-                cardNum = cardNum.substring(0, cardNum.length() - 1);
-
-            try {
-                card.quantity = Integer.parseInt(cardNum);
-            } catch (NumberFormatException e) {
-                System.out.println("NumberFormatException detected: " + e);
-                return null;
-            }
-
-            card.name = str.substring(1 + cardDelim);
-            fin.add(card);
-        }
-
-        return fin;
     }
 }
