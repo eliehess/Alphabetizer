4fa141e1fe580ce8eb601ecff06912173307421e
Elie <eliemhess@gmail.com>
2019-10-21 13:07:59 -0400

    First attempt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,216 @@
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.application.Application;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-import java.net.URL;
-
-public final class Main extends Application {
-    private static final int WIDTH = 750;
-    private static final int HEIGHT = 516 + 24;
+public final class Main {
+    private static List<String> errorLog;
 
     public static void main(String[] args) {
-        launch(args);
+        errorLog = new ArrayList<>();
+        String text = getClipboardContents();
+
+        if (text == null) {
+            errorLog.add("getClipboardContents() returned null");
+            printLog();
+            return;
+        }
+
+        text = alphabetize(text);
+
+        if (text == null || text.trim().length() < 1) {
+            errorLog.add("alphabetize() returned null");
+            printLog();
+            return;
+        }
+
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+                new StringSelection(text), null);
+        printLog();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        URL scene = getClass().getResource("/Main.fxml");
-        Parent loadingRoot = FXMLLoader.load(scene);
-
-        primaryStage.setResizable(false);
-        primaryStage.setMaxHeight(HEIGHT);
-        primaryStage.setMinHeight(HEIGHT);
-        primaryStage.setMaxWidth(WIDTH);
-        primaryStage.setMinWidth(WIDTH);
-        primaryStage.setTitle("Alphabetizer");
-        primaryStage.setScene(new Scene(loadingRoot));
-        primaryStage.show();
+    private static void printLog() {
+        if (errorLog.size() < 1) return;
+        try {
+            String str = "";
+            for (String logStr : errorLog)
+                str += logStr + "\n";
+            BufferedWriter writer = new BufferedWriter(new FileWriter("log.txt"));
+            writer.write(str);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("IOException detected: " + e);
+        }
     }
 
-    @Override
-    public void stop() {
-        System.exit(0);
+    private static String getClipboardContents() {
+        String fin = null;
+        Transferable contents = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+        if (contents == null) return null;
+
+        if (contents.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            try {
+                fin = (String) contents.getTransferData(DataFlavor.stringFlavor);
+            } catch (IOException e) {
+                errorLog.add("IOException detected: " + e.toString());
+            } catch (UnsupportedFlavorException e) {
+                errorLog.add("UnsupportedFlavorException detected: " + e.toString());
+            }
+        }
+        return fin;
+    }
+
+    private static class Card implements Comparable<Card> {
+        final String name;
+        final int quantity;
+
+        Card(String name, int quantity) {
+            this.name = name;
+            this.quantity = quantity;
+        }
+
+        @Override
+        public String toString() {
+            return quantity + " " + name;
+        }
+
+        @Override
+        public int compareTo(Card otherCard) {
+            return name.compareTo(otherCard.name);
+        }
+
+    }
+
+    /**
+     * Parses a String into a List of Cards, then alphabetizes the list,  merges duplicate entries,
+     * and converts it back into a String.
+     *
+     * @param input the String to be parsed and alphabetized.
+     * @return an alphabetized String.
+     */
+    private static String alphabetize(String input) {
+        List<Card> cards = new ArrayList<>();
+
+        final String delimiter = "&";
+        if (input.contains(delimiter)) {
+            errorLog.add("ERROR: Input contains prohibited character " + delimiter);
+            return null;
+        }
+
+        StringTokenizer tokens = new StringTokenizer(input.replaceAll("\\r", "")
+                .replaceAll("\\n", delimiter), delimiter);
+
+        int i = 0;
+        while (tokens.hasMoreTokens()) {
+            String str = tokens.nextToken();
+            int quantity;
+            String name;
+
+            final int splitPoint = str.indexOf(" ");
+            if (splitPoint <= 0) {
+                errorLog.add("ERROR: line " + i + " does not contain a space");
+                continue;
+            }
+
+            String cardNum = str.substring(0, splitPoint);
+            if (cardNum.substring(cardNum.length() - 1).equals("x"))
+                cardNum = cardNum.substring(0, cardNum.length() - 1);
+
+            try {
+                quantity = Integer.parseInt(cardNum);
+            } catch (NumberFormatException e) {
+                errorLog.add("Line " + i + "does not contain a properly-formatted number");
+                continue;
+            }
+
+            name = str.substring(1 + splitPoint);
+            cards.add(new Card(name, quantity));
+            i++;
+        }
+
+        cards = mergeSort(cards);
+        String fin = "";
+        for (Card card : cards)
+            fin += (card + "\n");
+        return fin;
+    }
+
+    /**
+     * Sorts a list of Cards using a mergesort algorithm.
+     *
+     * @param cards the list of Cards to be sorted.
+     * @return the sorted list.
+     */
+    private static List<Card> mergeSort(List<Card> cards) {
+        if (cards.size() < 2) return cards;
+
+        int leftSize = cards.size() / 2;
+        int rightSize = cards.size() - leftSize;
+        List<Card> leftList = new ArrayList<>();
+        List<Card> rightList = new ArrayList<>();
+
+        for (int i = 0; i < leftSize; i++)
+            leftList.add(cards.get(i));
+
+        for (int i = 0; i < rightSize; i++)
+            rightList.add(cards.get(i + leftSize));
+
+        return merge(mergeSort(leftList), mergeSort(rightList));
+    }
+
+    /**
+     * Merges two sorted list of Cards. Multiple Cards with the same name are combined into a single entry.
+     * Returns null if either input list is null. If one input list is empty, the other is returned.
+     *
+     * @param leftList  one list of Cards to be merged.
+     * @param rightList another list of Cards to be merged.
+     * @return the merged list.
+     */
+    private static List<Card> merge(List<Card> leftList, List<Card> rightList) {
+        if (leftList == null || rightList == null) {
+            errorLog.add("Attempting to merge null list");
+            return null;
+        } else if (leftList.size() == 0) return rightList;
+        else if (rightList.size() == 0) return leftList;
+
+        List<Card> fin = new ArrayList<>();
+        int leftCounter = 0;
+        int rightCounter = 0;
+        int leftSize = leftList.size();
+        int rightSize = rightList.size();
+        for (int i = 0; i < (leftSize + rightSize); i++) {
+            if (rightCounter >= rightSize && leftCounter >= leftSize) {
+                break;
+            } else if (rightCounter >= rightSize) {
+                fin.add(leftList.get(leftCounter));
+                leftCounter++;
+            } else if (leftCounter >= leftSize) {
+                fin.add(rightList.get(rightCounter));
+                rightCounter++;
+            } else {
+                Card leftElement = leftList.get(leftCounter);
+                Card rightElement = rightList.get(rightCounter);
+                int compare = leftElement.compareTo(rightElement);
+                if (compare == 0) {
+                    fin.add(new Card(leftElement.name, leftElement.quantity + rightElement.quantity));
+                    leftCounter++;
+                    rightCounter++;
+                } else if (compare < 0) {
+                    fin.add(leftElement);
+                    leftCounter++;
+                } else {
+                    fin.add(rightElement);
+                    rightCounter++;
+                }
+            }
+        }
+        return fin;
     }
 }
